d42cafac6be6602a69a8618921b184cdc9f4b370
tejas@merosy <tejas@merosy.lab>
2019-01-30 16:21:25 +0100

    Camera topic working

--- scripts/eyes_emergency.py
+++ scripts/eyes_emergency.py
@@ -4,6 +4,7 @@
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
 
 # import the necessary packages
+import sys
 from scipy.spatial import distance as dist
 from imutils.video import FileVideoStream
 from imutils.video import VideoStream
@@ -29,7 +30,8 @@ def frame_grabber(image):
     try:
         cv_image = bridge.imgmsg_to_cv2(image, "bgra8")
     except CvBridgeError as e:
-      print(e)
+        print(e)
+
     cv2.imshow("Frame", cv_image)
     key = cv2.waitKey(1) & 0xFF
 
@@ -37,6 +39,10 @@ def frame_grabber(image):
 
 def main():
     rospy.init_node("eyes_emergency")
+    rospy.sleep(1)
+    predictor = sys.argv[1]
+    rospy.loginfo("Using predictor : {}".format(predictor))
+
     rospy.Subscriber("/d435/ColorImage",Image, frame_grabber)
     rospy.spin()
 
